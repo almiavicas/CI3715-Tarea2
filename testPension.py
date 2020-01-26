843c21a5e9edf8305b1f744a2c9878898abc5cc3
alvaro <almiavicas@gmail.com>
2020-01-26 18:09:02 -0400

    Finished tests

--- testPension.py
+++ testPension.py
@@ -3,14 +3,18 @@ import math
 from datetime import date
 from pension import pension
 
+# Helper constants
 WEEKS_PER_YEAR = 52
-# Semanas necesarias para obtener la pension
 NEEDED_WEEKS = 750
+SUFFICIENT_YEARS = date.today().year - math.ceil(NEEDED_WEEKS / WEEKS_PER_YEAR)
+INSUFFICIENT_YEARS = date.today().year - math.floor(NEEDED_WEEKS / WEEKS_PER_YEAR)
+
 
 class TestPension(unittest.TestCase):
 
+
     def test_male_valid_worker(self):
-        year = date.today().year - math.ceil(NEEDED_WEEKS / WEEKS_PER_YEAR)
+        year = SUFFICIENT_YEARS
         month = date.today().month
         day = date.today().day
         workDate = date(year, month, day)
@@ -19,52 +23,66 @@ class TestPension(unittest.TestCase):
 
 
     def test_male_invalid_worker(self):
-        year = date.today().year - math.floor(NEEDED_WEEKS / WEEKS_PER_YEAR)
+        year = INSUFFICIENT_YEARS
         month = date.today().month
         day = date.today().day
         workDate = date(year, month, day)
-        birthDate = date(date.today().year - 62, month, day)
+        birthDate = date(date.today().year - 59, month, day)
         self.assertFalse(pension(workDate, birthDate, False, False))
 
 
-    # def test_female_valid_worker(self):
-    #     year = date.today().year - 58
-    #     month = date.today().month
-    #     day = date.today().day
-    #     workDate = date(year, month, day)
-    #     birthDate = date(year - 18, month, day)
-    #     self.assertTrue(pension(workDate, birthDate, False, True))
+    def test_female_valid_worker(self):
+        year = SUFFICIENT_YEARS
+        month = date.today().month
+        day = date.today().day
+        workDate = date(year, month, day)
+        birthDate = date(date.today().year - 56, month, day)
+        self.assertTrue(pension(workDate, birthDate, False, True))
 
 
-    # def test_female_invalid_worker(self):
-    #     year = date.today().year - 55
-    #     month = date.today().month
-    #     day = date.today().day
-    #     workDate = date(year, month - 1 % 12, day + 2 % 28)
-    #     birthDate = date(year - 28, month, day)
-    #     self.assertFalse(pension(workDate, birthDate, False, True))
+    def test_female_invalid_worker(self):
+        year = INSUFFICIENT_YEARS
+        month = date.today().month
+        day = date.today().day
+        workDate = date(year, (month - 1 % 12) + 1, (day + 2 % 28) + 1)
+        birthDate = date(date.today().year - 45, month, day)
+        self.assertFalse(pension(workDate, birthDate, False, True))
 
 
-    # def test_male_unhealthy_valid_worker(self):
-    #     year = 1964
-    #     month = 3
-    #     day = 14
-    #     workDate = date(year, month, day)
-    #     birthDate = date(year - 22, month + 3, day + 3)
-    #     self.assertTrue(pension(workDate, birthDate, True, False))
+    def test_male_unhealthy_valid_worker(self):
+        year = SUFFICIENT_YEARS - 20
+        month = date.today().month
+        day = date.today().day
+        workDate = date(year, month, day)
+        birthDate = date(date.today().year - 56, (month + 3 % 12) + 1, (day + 3 % 28) + 1)
+        self.assertTrue(pension(workDate, birthDate, True, False))
 
 
-    # def test_male_unhealthy_invalid_worker(self):
-    #     self.assertFalse(pension())
+    def test_male_unhealthy_invalid_worker(self):
+        year = INSUFFICIENT_YEARS
+        month = date.today().month
+        day = date.today().day
+        workDate = date(year, month, day)
+        birthDate = date(date.today().year - 54, month, day)
+        self.assertFalse(pension(workDate, birthDate, True, False))
 
 
-    # def test_female_unhealthy_valid_worker(self):
-    #     self.assertTrue(pension())
+    def test_female_unhealthy_valid_worker(self):
+        year = SUFFICIENT_YEARS - 20
+        month = date.today().month
+        day = date.today().day
+        workDate = date(year, month, day)
+        birthDate = date(date.today().year - 51, month, day)
+        self.assertTrue(pension(workDate, birthDate, True, True))
 
 
-    # def test_female_unhealthy_invalid_worker(self):
-    #     self.assertFalse(pension())
-
+    def test_female_unhealthy_invalid_worker(self):
+        year = INSUFFICIENT_YEARS
+        month = date.today().month
+        day = date.today().day
+        workDate = date(year, month, day)
+        birthDate = date(date.today() - 49, month, day)
+        self.assertFalse(pension(workDate, birthDate, True, True))
 
 if __name__ == '__main__':
     unittest.main()
