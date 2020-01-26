39ef2ac39b5a33653ddfa56798ebf8ccce230509
alvaro <almiavicas@gmail.com>
2020-01-26 16:31:47 -0400

    Defined 3 first tests

--- testPension.py
+++ testPension.py
@@ -5,34 +5,62 @@ from pension import pension
 class TestPension(unittest.TestCase):
 
     def test_male_valid_worker(self):
-        self.assertTrue(pension())
+        year = date.today().year - 70
+        month = date.today().month
+        day = date.today().day
+        workDate = date(year, month, day)
+        birthDate = date(year - 20, month, day)
+        self.assertTrue(workDate, birthDate, False, False)
 
 
     def test_male_invalid_worker(self):
-        self.assertFalse(pension())
+        year = date.today().year - 50
+        month = date.today().month
+        day = date.today().day
+        workDate = date(year, month, day)
+        birthDate = date(year - 20, month, day)
+        self.assertFalse(workDate, birthDate, False, False)
 
 
     def test_female_valid_worker(self):
-        self.assertTrue(pension())
+        year = date.today().year - 58
+        month = date.today().month
+        day = date.today().day
+        workDate = date(year, month, day)
+        birthDate = date(year - 18, month, day)
+        self.assertTrue(workDate, birthDate, False, True)
 
 
-    def test_female_invalid_worker(self):
-        self.assertFalse(pension())
+    # def test_female_invalid_worker(self):
+    #     year = date.today().year - 70
+    #     month = date.today().month
+    #     day = date.today().day
+    #     workDate = date(year, month, day)
+    #     birthDate = date(year - 20, month, day)
+    #     self.assertFalse(
+    #         pension(
+    #             date(date.today().year - 55, date.today().month - 1, date.today().day + 3),
+    #             date(date.today().year - 90, date.today().month, date.today().day),
+    #             False,
+    #             True
+    #         )
+    #     )
 
 
-    def test_male_unhealthy_valid_worker(self):
-        self.assertTrue(pension())
+    # def test_male_unhealthy_valid_worker(self):
+    #     self.assertTrue(pension())
 
 
-    def test_male_unhealthy_invalid_worker(self):
-        self.assertFalse(pension())
+    # def test_male_unhealthy_invalid_worker(self):
+    #     self.assertFalse(pension())
 
 
-    def test_female_unhealthy_valid_worker(self):
-        self.assertTrue(pension())
+    # def test_female_unhealthy_valid_worker(self):
+    #     self.assertTrue(pension())
 
-    def test_female_unhealthy_invalid_worker(self):
-        self.assertFalse(pension())
+
+    # def test_female_unhealthy_invalid_worker(self):
+    #     self.assertFalse(pension())
 
 
 if __name__ == '__main__':
